37648a6861c95d5f3a9c949e192cfd28c3936b82
Yashi1704 <141404716+Yashi1704@users.noreply.github.com>
2023-08-07 18:27:57 +0530

    Add files via upload

---
+++ yashica/compound.java
@@ -0,0 +1,8 @@
+public class compound {
+    public static void main(String[] args) {
+        int prin = 14, r = 2;
+        int n = 3, t = 5;
+        int A = prin*(1 + (r/n))^n*t;
+        System.out.println(A);
+    }
+}
